bb26999dba53eb1c955de80b3b2e1fe0d28d4775
aatosm <aatosm@hotmail.com>
2019-12-01 16:33:45 +0200

    Minor cleaning on day1 code

--- day1.ts
+++ day1.ts
@@ -1,5 +1,8 @@
 import * as fs from 'fs';
 
+const inputStrings = fs.readFileSync('./inputs/day1.txt').toString().split('\n');
+const input = inputStrings.map((i: string) => parseInt(i));
+
 const countFuel = (mass: number): number => {
   return Math.floor(mass / 3) - 2;
 };
@@ -9,14 +12,11 @@ const countFuelRecursive = (mass: number): number => {
   return fuel <= 0 ? 0 : fuel + countFuelRecursive(fuel);
 }
 
-const inputStrings = fs.readFileSync('./inputs/day1.txt').toString().split('\n');
-const input = inputStrings.map((i: string) => parseInt(i));
-
 const resultPartOne = input.reduce((counter: number, current: number) =>
-counter + countFuel(current), 0);
+  counter + countFuel(current), 0);
 
 const resultPartTwo = input.reduce((counter: number, current: number) =>
-counter + countFuelRecursive(current), 0);
+  counter + countFuelRecursive(current), 0);
 
 console.log(resultPartOne);
 console.log(resultPartTwo);
